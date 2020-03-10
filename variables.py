342c441601d08e5440e8852b4a1268392a9ebe9b
richiewesh <rmaina913@gmail.com>
2020-03-10 17:19:36 +0300

    learning variables and strings

---
+++ variables.py
@@ -0,0 +1,17 @@
+firstName="richard"
+lastName="maina"
+favNum = "4"
+
+student = True
+employed = False
+name = "techcamp"
+name = "techcamp"
+
+print(type(firstName))
+print(type(lastName))
+print(type(favNum))
+# print(student)
+print(employed)
+print(name[3])
+print(name[-5])
+print(name[-4:])
