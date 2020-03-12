14817ad43e49eb7fe9c755504c4842f525b371e4
richiewesh <rmaina913@gmail.com>
2020-03-12 17:48:34 +0300

    python operators updated

---
+++ operators.py
@@ -0,0 +1,17 @@
+myDb=('13' , '7')
+
+
+un=input(' enter ticket no: ')
+
+
+print(type(un))
+
+un2=int(un)
+
+print(type(un2))
+
+if un2== 13 or un2==7:
+    print('enter to my bash')
+else:
+    print('thie ukiumaga')
+
